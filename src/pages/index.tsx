0231d0d279db0f811ec9cc47b78e6f5bab455944
harutete <ai.takeuchi.10@gmail.com>
2021-09-18 23:46:40 +0900

    Add: firebase

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Fuku</title>
+      </Head>
+
+      <main>
+        <h1>Fuku</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
