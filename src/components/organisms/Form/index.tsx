7c28c60642e5d4a01d32ce73dac37705dde25d3b
harutete <ai.takeuchi.10@gmail.com>
2021-09-22 22:52:08 +0900

    フォームのコンポーネント化

---
+++ src/components/organisms/Form/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from  './index.module.css'
+
+interface Props {
+  onSubmit: any;
+  email: string;
+  handleSetEmail: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  password: string;
+  handleSetPassword: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  submitValue: string
+}
+
+export const Form: React.FC<Props> = ({ onSubmit, email, handleSetEmail, password, handleSetPassword, submitValue }) => (
+  <div className={styles.formWrapper}>
+    <form onSubmit={onSubmit}>
+      <ul className={styles.formList}>
+        <li>
+          <input type="text" placeholder="email" value={email} onChange={handleSetEmail} />
+        </li>
+        <li>
+        <input type="password" placeholder="password" value={password} onChange={handleSetPassword} />
+        </li>
+      </ul>
+      <div className={styles.buttonSubmitWrap}>
+        <button type="submit" className={styles.buttonSubmit}>{submitValue}</button>
+      </div>
+    </form>
+  </div>
+)
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,6 +7,10 @@ import { useRouter } from 'next/router'
 import { loginApp } from '../../libs/firebase/auth'
 import { useInput } from '../../hooks/useInput'
 
+import { Form } from '../../components/organisms/Form'
+
+import styles from './index.module.css'
+
 const Login: NextPage = () => {
   const { value: email, handleSetValue: handleSetEmail } = useInput('')
   const { value: password, handleSetValue: handleSetPassword } = useInput('')
@@ -29,18 +33,7 @@ const Login: NextPage = () => {
 
       <main>
         <h1>Login</h1>
-        <div>
-          <form onSubmit={handleLogin}>
-            <div>
-              <input type="text" placeholder="email" value={email} onChange={handleSetEmail} />
-            </div>
-            <div>
-              <input type="password" placeholder="password" value={password} onChange={handleSetPassword} />
-            </div>
-            <input type="submit" value="Login" />
-          </form>
-          <p><Link href="/sign-in"><a>Create new account.</a></Link></p>
-        </div>
+        <Form onSubmit={handleLogin} email={email} handleSetEmail={handleSetEmail} password={password} handleSetPassword={handleSetPassword} submitValue="Login" />
       </main>
     </div>
   )
