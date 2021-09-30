3edf310c1387e7f8519a6ff00a8f787ef302a00f
harutete <ai.takeuchi.10@gmail.com>
2021-09-30 23:51:43 +0900

    Add: logout

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,9 +1,16 @@
 import styles from  './index.module.css'
+type Props = {
+  isLoggedIn: boolean
+  handleLogout?: () => Promise<void>
+}
 
-export const Header: React.FC = () => (
+export const Header: React.FC<Props> = ({ isLoggedIn, handleLogout }) => (
   <header className={styles.header}>
     <div className={styles.headerContent}>
       <div className={styles.headerTitle}>Fu-cloud</div>
+      {isLoggedIn &&
+        <div className={styles.logoutButton} onClick={handleLogout}>Logout</div>
+      }
     </div>
   </header>
 )
--- src/components/templates/DefaultLayout/index.tsx
+++ src/components/templates/DefaultLayout/index.tsx
@@ -1,3 +1,7 @@
+import { useRouter } from 'next/router'
+
+import { logoutApp } from '../../../libs/firebase/auth'
+
 import { Header } from '../../organisms/Header'
 import { MainWrapper } from '../../organisms/MainWrapper'
 import { Footer } from '../../organisms/Footer'
@@ -8,10 +12,18 @@ type Props = {
   children: React.ReactNode
 }
 
-export const DefaultLayout: React.FC<Props> = ({ children }) => (
-  <div className={styles.wrapper}>
-    <Header />
-    <MainWrapper>{children}</MainWrapper>
-    <Footer />
-  </div>
-)
+export const DefaultLayout: React.FC<Props> = ({ children }) => {
+  const router = useRouter()
+  const handleLogoutApp = async () => {
+    await logoutApp()
+    router.push('/login')
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <Header isLoggedIn={true} handleLogout={handleLogoutApp()} />
+      <MainWrapper>{children}</MainWrapper>
+      <Footer />
+    </div>
+  )
+}
